4272f5829b9a0a348058ef4b74524cc99c4a78b6
saikiran2001 <bjsaikiran@gmail.com>
2021-02-10 07:02:54 +0530

    Revert "CCodec: GraphicBufferSourceWrapper: fix to apply nBufferCountActual for deciding number of inputs"
    This reverts commit e2ce7ecad5f4cb96a2c840e3aff0d9174c2047ed.

--- media/codec2/sfplugin/CCodec.cpp
+++ media/codec2/sfplugin/CCodec.cpp
@@ -246,19 +246,8 @@ public:
         if (source == nullptr) {
             return NO_INIT;
         }
-
-        size_t numSlots = 4;
-        constexpr OMX_U32 kPortIndexInput = 0;
-
-        OMX_PARAM_PORTDEFINITIONTYPE param;
-        param.nPortIndex = kPortIndexInput;
-        status_t err = mNode->getParameter(OMX_IndexParamPortDefinition,
-                                           &param, sizeof(param));
-        if (err == OK) {
-            numSlots = param.nBufferCountActual;
-        }
-
-        for (size_t i = 0; i < numSlots; ++i) {
+        constexpr size_t kNumSlots = 16;
+        for (size_t i = 0; i < kNumSlots; ++i) {
             source->onInputBufferAdded(i);
         }
 
